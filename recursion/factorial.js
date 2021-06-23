e4427d8acb93526c19c14ec9777295613993c91b
Akash Singh <53407198+biharihu@users.noreply.github.com>
2021-06-23 14:17:30 +0530

    added recursion

---
+++ recursion/factorial.js
@@ -0,0 +1,19 @@
+function findFactorialRecursive(number) {
+  if (number === 2) {
+    return 2;
+  }
+  return number * findFactorialRecursive(number - 1);
+}
+
+findFactorialRecursive(5);
+
+function findFactorialIterative(number) {
+  let answer = 1;
+  if (number === 2) {
+    answer = 2;
+  }
+  for (let i = 2; i <= number; i++) {
+    answer = answer * i;
+  }
+  return answer;
+}
